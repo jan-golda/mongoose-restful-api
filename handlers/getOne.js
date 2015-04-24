b7a60e13d44e506b23249589e1ad2a5e47f41920
regzand <regzand@gmail.com>
2015-04-24 22:04:11 +0200

    Added base getOne handling

--- handlers/getOne.js
+++ handlers/getOne.js
@@ -1,6 +1,21 @@
 module.exports = function(model, options){
 
 	return function(req, res){
-		res.status(404).end(); // TODO: create actual hanler
+
+		// getting element
+		model.findOne()
+			.where('_id').eq(req.params.id)
+			.exec(function(err, item){
+				// throwing database errors
+				if(err)
+					throw err;
+
+				// sending 'not found' if item not exists
+				if(!item)
+					return res.status(404).end();
+
+				// sending item
+				res.status(200).json(item).end();
+			})
 	};
 };
