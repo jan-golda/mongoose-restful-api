d5859e90e7e970d2a53b2d83ea85551fe0843f08
regzand <regzand@gmail.com>
2015-04-24 21:55:25 +0200

    Created handlers

---
+++ handlers/getAll.js
@@ -0,0 +1,6 @@
+module.exports = function(model, options){
+
+	return function(req, res){
+		res.status(404).end(); // TODO: create actual hanler
+	};
+};
---
+++ handlers/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+	getAll:		require('./getAll'),
+	updateAll:	require('./updateAll'),
+	create:		require('./create'),
+	deleteAll:	require('./deleteAll'),
+	getOne:		require('./getOne'),
+	updateOne:	require('./updateOne'),
+	deleteOne:	require('./deleteOne')
+};
