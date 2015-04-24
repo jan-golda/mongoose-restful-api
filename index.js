a980a5bb706973e918af080cb5ba90b1d81557c0
regzand <regzand@gmail.com>
2015-04-24 21:32:00 +0200

    Project initialization

---
+++ index.js
@@ -0,0 +1,14 @@
+// public modules import
+var utils   = require('utils')._;
+
+// default options
+var defOptions = {
+
+};
+
+// exports
+module.exports = function(model, options){
+
+    // default options
+    options = utils.defaults({}, defOptions, options);
+};
