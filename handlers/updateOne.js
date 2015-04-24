826200a42bae2d766131d47517cf94644e0df24c
regzand <regzand@gmail.com>
2015-04-24 22:25:51 +0200

    Added base updateOne handling

--- handlers/updateOne.js
+++ handlers/updateOne.js
@@ -1,6 +1,21 @@
 module.exports = function(model, options){
 
 	return function(req, res){
-		res.status(404).end(); // TODO: create actual hanler
+
+		// updating element by id
+		model.findOneAndUpdate(req.body)
+			.where('_id').eq(req.params.id)
+			.exec(function(err, item){
+				// throwing database errors
+				if(err)
+					throw err;
+
+				// send 'not found' if item not exists
+				if(!item)
+					return res.status(404).end();
+
+				// sending success
+				res.status(200).end();
+			});
 	};
 };
