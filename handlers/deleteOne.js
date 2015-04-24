e4bb5c7b9c9697d37da2be162e49436122821122
regzand <regzand@gmail.com>
2015-04-24 22:12:18 +0200

    Added base deleteOne handling

--- handlers/deleteOne.js
+++ handlers/deleteOne.js
@@ -1,6 +1,21 @@
 module.exports = function(model, options){
 
 	return function(req, res){
-		res.status(404).end(); // TODO: create actual hanler
+
+		// removing item by id
+		model.findOneAndRemove()
+			.where('_id').eq(req.params.id)
+			.exec(function(err, item){
+				// throwing database errors
+				if(err)
+					throw err;
+
+				// sending 'not found' id element not exists
+				if(!item)
+					return res.status(404).end();
+
+				// sending success code
+				res.status(200).end();
+			});
 	};
 };
